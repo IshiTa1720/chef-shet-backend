233ae68171bab139b76a9954636c7ce9b2aba05f
Ishita Thakur <ishi.ta1700thakur@gmail.com>
2024-07-04 17:38:31 +0530

    created empty springboot project

---
+++ src/main/java/com/shin/chef/shet/ChefShetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shin.chef.shet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefShetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefShetBackendApplication.class, args);
+	}
+
+}
